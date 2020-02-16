0a39f157c1bc7b3d925bcb4be6db60933c0268bd
test1 <test1@naver.com>
2020-02-16 18:15:57 +0900

    20200209_1

--- st01.Python기초/py08반복문/py08_05_2단세로출력.py
+++ st01.Python기초/py08반복문/py08_05_2단세로출력.py
@@ -1,9 +1,7 @@
+
 x = 2
-for x in range(1,10,1) :
-    print("2 * ", x, "=", 2*x )
-#
+for x in range(1, 10, 1):
+    print("2 * ", x, "=", 2*x)
 
- x = 2
-for x in range(1,10,1) :
-    print("2 * %s = %s" % (x, 2*x))  #%d를 사용하여도 됨.
-
+for x in range(1, 10, 1):
+    print("2 * %s = %s" % (x, 2*x))  # %d를 사용하여도 됨.
